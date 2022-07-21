a1ef4c96dbb43982703b4fcc43c3a3b2ac53f6ec
anantKC <ananta007kc@gmail.com>
2022-07-21 16:29:15 +0545

    second message

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,153 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App" :class="typeof weather.main != 'undefined' && weather.main.temp > 20? 'warm':''" v-cloak>
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search.." v-model = "query" @keypress="fetchWeather" />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{weather.name}},{{weather.sys.country}}</div>
+          <div class="date">{{dateBuilder()}}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{Math.round(weather.main.temp)}}&#176;C</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      api_key:'1913a548fdd8dcc38aa6060e4bda175d',
+      url_base:'https://api.openweathermap.org/data/2.5/',
+      query:'',
+      weather:{}
+    }
+  },
+  methods:{
+    fetchWeather (e){
+      if(e.key=="Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results;
+    },
+    dateBuilder (){
+      let d = new Date();
+      let months = ["Janaury","Febraury","March","April","May","June","July","August","September","October","November","December"];
+      let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear(); 
+      return `${day} ${date} ${month} ${year}`;
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  [v-cloak]{
+    display:none;
+  }
+  *{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+  body{
+    font-family: 'montserrat',sans-serif;
+
+  }
+  #App{
+    background-image: url('./assets/cold_bg.jpg');
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+  }
+
+  #App.warm {
+    background-image: url(./assets/warm_back.jpg);
+  }
+  main{
+    min-height: 100vh;
+    padding: 25px;
+    background-image:linear-gradient(to bottom,rgba(0,0,0,0.25),rgba(0,0,0,0.75))
+  }
+  .search-box .search-bar{
+    display: block;
+    width: 50%;
+    padding: 15px;
+    margin: auto;
+    margin-bottom: 30px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 16px 16px 16px 16px;
+    transition: 0.4s;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  }
+  /* .search-box .search-bar:focus{
+    background-color: rgba(255, 255, 255, 0.75);
+    box-shadow: rgba(0, 0, 0, 0.75);
+    border-radius: 16px 0px 16px 0px;
+  } */
+  .location-box .location{
+    color: #fff;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+    font-family: fantasy;
+  }
+  .location-box .date{
+    color: #fff;
+    font-size: 32px;
+    font-weight: 300;
+    text-align: center;
+    font-style: italic;
+    font-family: fantasy;
+  }
+  .weather-box{
+    text-align: center;
+    
+  }
+  .weather-box .temp{
+    display: inline-block;
+    font-family: fantasy;
+    padding: 10px 25px;
+    color: #fff;
+    font-size: 102px;
+    font-weight: 900;
+    text-shadow:3px 6px rgba(0, 0, 0, 0.25) ;
+    background-color:rgba(255, 255, 255, 0.25) ;
+    border-radius: 10px;
+    margin: 30px 0px;
+    box-shadow: rgba(0, 0, 0, 0.25);
+  }
+  .weather-box .weather{
+    color: #fff;
+    font-size: 48px;
+    font-family: fantasy;
+    font-weight: 700;
+    text-align: center;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+
 </style>
